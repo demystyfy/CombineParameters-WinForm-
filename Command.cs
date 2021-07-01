312fbb2a58ec62a50517de8a8975f691ad2b2ac2
demystyfy <fylypof@gmail.com>
2021-07-01 14:49:25 +0300

    Add project files.

---
+++ Command.cs
@@ -0,0 +1,37 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+#endregion
+
+namespace CombineParametersWinForm
+{
+
+    [Transaction(TransactionMode.Manual)]
+
+
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message,
+         ElementSet elements)
+        
+        {
+
+        }
+
+
+
+
+
+    }
+
+
+}
